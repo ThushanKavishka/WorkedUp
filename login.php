ba41d00f899e67ba8ca97046849bdf281861f10c
THUSHAN <thushankavishka@gmail.com>
2017-03-05 07:23:02 +0530

    Change variable in login.php in lines 3

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php
 //create a variable called $pagename which contains the actual name of the page
-$pagename="Login";
+$pagenameLogin="Login";
 
 //call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
 //display window title
-echo "<title>".$pagename."</title>";
+echo "<title>".$pagenameLogin."</title>";
 //include head layout 
 include ("headfile.html");
 
 echo "<p></p>";
 //display name of the page and some random text
-echo "<h2>".$pagename."</h2>";
+echo "<h2>".$pagenameLogin."</h2>";
 echo "<p> Please Login";
 
 //include head layout
